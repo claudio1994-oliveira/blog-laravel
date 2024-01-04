17ed7e6fa2687a008b957b2c03dc5ff05517a236
Cláudio Oliveira <franciscoclaudiooliveira@gmail.com>
2024-01-04 11:23:36 -0300

    creating livewire components

---
+++ app/Livewire/Post/Edit.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Post;
+
+use Livewire\Component;
+
+class Edit extends Component
+{
+    public function render()
+    {
+        return view('livewire.post.edit');
+    }
+}
