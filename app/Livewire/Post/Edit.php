96ef09422b3ab87d9788c890a0a610d81a954d18
Cláudio Oliveira <franciscoclaudiooliveira@gmail.com>
2024-01-04 17:20:13 -0300

    updating posts

--- app/Livewire/Post/Edit.php
+++ app/Livewire/Post/Edit.php
@@ -3,6 +3,8 @@
 namespace App\Livewire\Post;
 
 use App\Models\Post;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Routing\Redirector;
 use Illuminate\View\View;
 
 class Edit extends PostBase
@@ -17,5 +19,16 @@ class Edit extends PostBase
     public function render(): View
     {
         return view('livewire.post.edit');
+    }
+
+    public function update(): RedirectResponse|Redirector
+    {
+        $this->validate();
+
+        $this->post->update($this->fields);
+
+        session()->flash('success', 'Post successfully updated.');
+
+        return to_route('dashboard');
     }
 }
